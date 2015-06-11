3fafddc441bc0a0095afe2f5699ad1fa69e2eb40
chandler1142 <thzw1142@163.com>
2015-06-11 09:48:05 +0800

    add odl.jsp and add a table to it

--- WebContent/WEB-INF/view/odl.jsp
+++ WebContent/WEB-INF/view/odl.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,7 +8,28 @@
 </head>
 <body>
 	<%@include file="../../head.jsp"%>
-	
-	<c:out value="${response}"></c:out>
+
+
+	<div>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>topology-id</th>
+					<th>node-id</th>
+
+				</tr>
+			</thead>
+			<tbody>
+				<c:forEach var="node" items="${nodeList }">
+					<tr>
+						<th><c:out value="${topology_id}"></c:out></th>
+						<td>${node}</td>
+					</tr>
+				</c:forEach>
+
+
+			</tbody>
+		</table>
+	</div>
 </body>
 </html>
--- src/com/odl/controller/ODL_Render.java
+++ src/com/odl/controller/ODL_Render.java
@@ -1,5 +1,11 @@
 package com.odl.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +21,6 @@ public class ODL_Render {
 
 	ODLService service = new ODLService();
 
-	
 	@RequestMapping(value = "/odl", method = RequestMethod.GET)
 	public String render(Model model, @RequestParam("url") String url) {
 		if (url.equals("") || url == "") {
@@ -31,15 +36,39 @@ public class ODL_Render {
 				return "error";
 			} else {
 				String response = topo.odl_http_get("GET", api,
-						PropUtil.getProperty("contentType"),
-						null,
+						PropUtil.getProperty("contentType"), null,
 						PropUtil.getProperty("accept"), "200");
 				System.out.println(response);
-				model.addAttribute("response", response);
+
+				try {
+					List<String> list = new ArrayList<String>();
+					JSONObject jsonObject = new JSONObject(response);
+					String topology_id = "";
+					// JSONArray jsonArray =
+					// jsonObject.getJSONArray("network-topology");
+					JSONObject topoObject = (JSONObject) jsonObject
+							.get("network-topology");
+					JSONArray topologyArray = topoObject
+							.getJSONArray("topology");
+
+					JSONObject obj = (JSONObject) topologyArray.get(0);
+
+					topology_id = obj.getString("topology-id");
+					JSONArray array = obj.getJSONArray("node");
+					for(int i=0;i<array.length();i++) {
+						JSONObject o = array.getJSONObject(i);
+						list.add(o.getString("node-id"));
+					}
+					model.addAttribute("topology_id", topology_id);
+					model.addAttribute("nodeList",list);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
 			}
 		}
 
 		return "odl";
 	}
-	
+
 }
